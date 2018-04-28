74bb4d59516466dd77fd59972157815e2e5f6b9a
Jesse Plamondon-Willard <github@jplamondonw.com>
2018-04-27 21:26:48 -0400

    update for Stardew Valley 1.3

--- FastTravel/FastTravelUtils.cs
+++ FastTravel/FastTravelUtils.cs
@@ -17,7 +17,7 @@ namespace FastTravel
         /// <returns></returns>
         public static bool PointExistsInConfig(ClickableComponent point)
         {
-            return ModEntry.Config.FastTravelPoints.Any(t => point.name.StartsWith(t.MapName.Replace("{0}", Game1.player.farmName)));
+            return ModEntry.Config.FastTravelPoints.Any(t => point.name.StartsWith(t.MapName.Replace("{0}", Game1.player.farmName.Value)));
         }
 
         /// <summary>
@@ -28,7 +28,7 @@ namespace FastTravel
         public static GameLocation GetLocationForMapPoint(ClickableComponent point)
         {
             string pointName = point.name;
-            return Game1.locations[ModEntry.Config.FastTravelPoints.First(t => pointName.StartsWith(t.MapName.Replace("{0}", Game1.player.farmName))).GameLocationIndex];
+            return Game1.locations[ModEntry.Config.FastTravelPoints.First(t => pointName.StartsWith(t.MapName.Replace("{0}", Game1.player.farmName.Value))).GameLocationIndex];
         }
 
         /// <summary>
@@ -38,8 +38,8 @@ namespace FastTravel
         /// <returns></returns>
         public static FastTravelPoint GetFastTravelPointForMapPoint(ClickableComponent point)
         {
-            string pointName = point.name.Replace("{0}", Game1.player.farmName);
-            return ModEntry.Config.FastTravelPoints.First(t => pointName.StartsWith(t.MapName.Replace("{0}", Game1.player.farmName)));
+            string pointName = point.name.Replace("{0}", Game1.player.farmName.Value);
+            return ModEntry.Config.FastTravelPoints.First(t => pointName.StartsWith(t.MapName.Replace("{0}", Game1.player.farmName.Value)));
         }
     }
 }
